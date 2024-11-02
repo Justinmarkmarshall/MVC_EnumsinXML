b195c7aa1eb7afba09673356818a04f9eb77d880
Justin Marshall <justinmarkmarshall1@gmail.com>
2024-11-02 12:33:08 +0000

    add json serializer options

--- SerializationDemo/Program.cs
+++ SerializationDemo/Program.cs
@@ -1,10 +1,16 @@
 using SerializationDemo.Components.Implementation;
 using SerializationDemo.Components.Interfaces;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews().AddXmlSerializerFormatters();
+builder.Services.AddControllersWithViews().AddXmlSerializerFormatters()
+    .AddJsonOptions(options =>
+    {
+        // add this to make sure that the enums are serialized as strings
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 builder.Services.AddTransient<IStatusService, StatusService>();
 
 var app = builder.Build();
